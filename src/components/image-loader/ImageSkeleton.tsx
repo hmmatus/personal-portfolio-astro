632ac66a057304a7f9cd757962576286ffe6bf1b
Hector Matus <hmatus.dev@gmail.com>
2025-03-18 20:54:02 -0600

    feat: Added skeleton to big size images and a skeleton for loading

---
+++ src/components/image-loader/ImageSkeleton.tsx
@@ -0,0 +1,12 @@
+import Skeleton from "react-loading-skeleton";
+
+export const ImageSkeleton = () => {
+  return (
+    <Skeleton
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    />
+  );
+};
